baf02f7a293ac924311f62db172c4c65216153e2
Sadia Tashnim <71938476+sadiatashnim@users.noreply.github.com>
2020-10-01 13:02:21 +0600

    CSE Assignment-02

---
+++ Assignment-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+
+    int row,col,count=1;
+    for(row=1;row<=5;row++){
+
+        for(col=1;col<=row;col++)
+        printf("%d ", count++);
+        printf("\n");
+    }
+}
